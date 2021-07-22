15885abfebb40b58f31f6247dc6bab8bd3eb3d35
middle-frostfire <fdz.fhz1302@gmail.com>
2021-07-22 20:17:45 +0700

    Initial Commit

---
+++ Capmeal/CapmealApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapmealApp.swift
+//  Capmeal
+//
+//  Created by Fahmi Dzulqarnain on 22/07/21.
+//
+
+import SwiftUI
+
+@main
+struct CapmealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

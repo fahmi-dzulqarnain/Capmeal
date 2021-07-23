3e1635f36c928ce36786a169c78c2b26145848bf
erikson45 <43662385+erikson45@users.noreply.github.com>
2021-07-23 17:14:18 +0700

    create starter project + create view coreml model script

---
+++ Capmeal/ViewModel/CoreMLViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  CoreMLViewModel.swift
+//  Capmeal
+//
+//  Created by Ericson Hermanto on 23/07/21.
+//
+
+import Foundation
+import CoreML
+import Vision
+
+class CoreMLVievModel : ObservableObject {
+    
+}
